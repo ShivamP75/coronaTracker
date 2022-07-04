8f2d2b67932d776cd969ccf69c65b0350da9e79a
ShivamP75 <99608687+ShivamP75@users.noreply.github.com>
2022-07-04 13:17:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let coronaTracker = {
+    url : "https://api.covid19api.com/summary",
+
+    getUpdate : async function(){
+        let response = await fetch(this.url);
+        if(!(response.ok == true)){
+            alert("Please enter correct contry name");
+        }
+        else{
+            let data = await response.json();
+            return data;
+        }
+    },
+    updateData : function(){
+
+        let data = this.getUpdate();
+        data.then(data1 => {
+             document.querySelector('#death').innerText = data1.Global.TotalDeaths;
+            document.querySelector('#confirmed').innerText = data1.Global.NewConfirmed;
+            document.querySelector('#recoverd').innerText = data1.Global.TotalRecovered;
+        })
+        
+
+    },
+   
+    }
+    // search : function(newUrl)
+
+
+coronaTracker.updateData();
+
+document.querySelector('#refresh').addEventListener('click', () => {
+        this.coronaTracker.updateData();
+})
+
+
